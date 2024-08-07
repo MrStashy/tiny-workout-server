a4eb3f15121c42db196d148ad7b5d6832b28e421
Will Baxter <bax.will@googlemail.com>
2024-08-07 15:19:47 +0100

    Express server initialised

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const app = require('./server')
+
+
+
+const port = process.env.EXPRESS_PORT
+
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
---
+++ src/server.js
@@ -0,0 +1,13 @@
+const { config } = require('dotenv');
+config();
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+module.exports =  app 
