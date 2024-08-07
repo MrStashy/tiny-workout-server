40d85a282f5fa8160fd6ec33778eab09e34d1cf9
Will Baxter <bax.will@googlemail.com>
2024-08-07 17:01:42 +0100

    Tests and db initialised

---
+++ src/utils/prisma.js
@@ -0,0 +1,10 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+if (process.env.NODE_ENV === 'test') {
+    process.env['DATABASE_URL'] = process.env['TEST_DATABASE_URL']
+    console.log(`Connected to DB instance: ${process.env['DATABASE_URL']}`)
+}
+
+
+export default prisma
